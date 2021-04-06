8dde5d63f0211e96d1990b552af48e230bed906e
Daniel Kasprów <127341@student.uni.opole.pl>
2021-04-06 17:46:59 +0200

    zad 1 patch

--- lista_5/src/zad1/tab_mnozenia.java
+++ lista_5/src/zad1/tab_mnozenia.java
@@ -7,8 +7,16 @@ public class tab_mnozenia {
 	public static void main(String[] args) throws ParseException {
 	
 		Scanner sc = new Scanner(System.in);
+		int n=0;
+		do
+		{
 		System.out.print("Podaj n: ");
-		int n = sc.nextInt();
+		try
+		{
+			n = sc.nextInt();
+		}
+		catch(NumberFormatException n1) {}
+		}while(n<=0);
 		sc.close();
 		
 		String wypisanieLinijki=" ";
@@ -19,29 +27,45 @@ public class tab_mnozenia {
 		{
 			for(int y=0;y<=n;y++)
 			{
-				int k=n*y;
-				String lenghtMax = "" + k;
-				k=lenghtMax.length()+1;
-				int l=x*y;
-				String lenghtNumber = "" + l;
-				l=lenghtNumber.length();
-				for(int g=0;g<(k-l);g++)
-				{
-					wypisanieLinijki+=" ";
-				}
 				if(x==0 && y==0)
+				{
+					wypisanieLinijki = spacje(wypisanieLinijki,1,1,n);
 					wypisanieLinijki+="  ";
+				}
 				else if(x==0)
+				{
+					wypisanieLinijki = spacje(wypisanieLinijki,1,y,n);
 					wypisanieLinijki+=" " + y;
+				}
 				else if(y==0)
-					wypisanieLinijki+=" " +x;
+				{
+					wypisanieLinijki = spacje(wypisanieLinijki,x,1,n);
+					wypisanieLinijki+=" " + x;
+				}
 				else
+				{
+					wypisanieLinijki = spacje(wypisanieLinijki,x,y,n);
 					wypisanieLinijki+=" " +(x*y);
+				}
 			}
 			System.out.print("\n" + wypisanieLinijki);
 			wypisanieLinijki=" ";
 		}
 		
+	}
+	static String spacje(String wypisanieLinijki,int x,int y,int n)
+	{
+		int k=n*y;
+		String lenghtMax = "" + k;
+		k=lenghtMax.length()+1;
+		int l=x*y;
+		String lenghtNumber = "" + l;
+		l=lenghtNumber.length();
+		for(int g=0;g<(k-l);g++)
+		{
+			wypisanieLinijki+=" ";
+		}
+		return wypisanieLinijki;
 	}
 
 }
